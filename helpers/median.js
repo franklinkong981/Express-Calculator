31ae6ca2f6d57e3324d98ba6081bf26d56e17613
Franklin Kong <franklinkong981@gmail.com>
2024-08-04 12:37:43 -0700

    Finished and tested all routes

--- helpers/median.js
+++ helpers/median.js
@@ -1,14 +1,14 @@
 function calculateMedian(nums) {
-    nums.sort();
-    if (nums.length % 2 != 0) {
+    sortedNums = nums.sort(function (a, b) {  return a - b;  });
+    if (sortedNums.length % 2 != 0) {
         //length of nums is odd
-        return nums[Math.floor(nums.length / 2)];
+        return sortedNums[Math.floor(nums.length / 2)];
     }
     else {
         //length of nums is even
-        let secondIndex = nums.length / 2;
+        let secondIndex = sortedNums.length / 2;
         let firstIndex = secondIndex - 1;
-        return (nums[firstIndex] + nums[secondIndex]) / 2;
+        return (sortedNums[firstIndex] + sortedNums[secondIndex]) / 2;
     }
 }
 
